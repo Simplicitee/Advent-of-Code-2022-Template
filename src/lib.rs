694993e30ede071573b3036daee6e0a811079dcf
Benford Whitaker <simplicitee123@gmail.com>
2022-12-02 22:28:43 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,19 @@ use std::{io::{Lines, BufReader}, fs::File};
 // Use this function to solve part one of the puzzle
 // Return the answer as a String
 pub fn part_one(lines: Lines<BufReader<File>>) -> String {
+    for line in lines {
+        if let Ok(contents) = line {
+            // do something with each line :)
+        }
+    }
+
     "Not Started".to_owned()
 }
 
 // Use this function to solve part two of the puzzle
-// Probably copy/paste the part one code to modify
 // Return the answer as a String
 pub fn part_two(lines: Lines<BufReader<File>>) -> String {
+    // Recommend copy/paste the part one code to modify
+
     "Not Started".to_owned()
 }
