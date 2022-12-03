501fdfa6e8c10fdc2b12c56481931a203ca622eb
Benford Whitaker <simplicitee123@gmail.com>
2022-12-02 22:05:26 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use std::{io::{Lines, BufReader}, fs::File};
+
+// Make sure to put both input datas into their files!
+
+// Use this function to solve part one of the puzzle
+// Return the answer as a String
+pub fn part_one(lines: Lines<BufReader<File>>) -> String {
+    "Not Started".to_owned()
+}
+
+// Use this function to solve part two of the puzzle
+// Probably copy/paste the part one code to modify
+// Return the answer as a String
+pub fn part_two(lines: Lines<BufReader<File>>) -> String {
+    "Not Started".to_owned()
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::{io::{Lines, BufReader, BufRead}, fs::File, path::Path};
+use lib::*;
+
+mod lib;
+
+// Make sure to put both input datas into their files!
+
+fn main() {
+    println!("Test Answers: (do I match the example from the puzzle?)");
+    println!("- Part 1: {}", part_one(read_lines("src/inputs/example.txt")));
+    println!("- Part 2: {}", part_two(read_lines("src/inputs/example.txt")));
+    println!();
+
+    println!("Answers:");
+    println!("- Part 1: {}", part_one(read_lines("src/inputs/solving.txt")));
+    println!("- Part 2: {}", part_two(read_lines("src/inputs/solving.txt")));
+}
+
+fn read_lines<P>(filename: P) -> Lines<BufReader<File>>
+where P: AsRef<Path>, {
+    let file = match File::open(filename) {
+        Err(why) => panic!("couldn't open file: {}", why),
+        Ok(file) => file,
+    };
+    BufReader::new(file).lines()
+}
