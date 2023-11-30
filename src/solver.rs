7b80cce7d0a8b40b9d83019e74ef6d074797fb3b
Benford Whitaker <simplicitee123@gmail.com>
2023-11-30 12:27:17 -0500

    Update solver.rs

--- src/solver.rs
+++ src/solver.rs
@@ -8,20 +8,20 @@ use std::{io::{Lines, BufReader}, fs::File};
 
 // Use this function to solve part one of the puzzle
 // Return the answer as a string
-pub fn part_one(lines: Lines<BufReader<File>>) -> &'static str {
+pub fn part_one(lines: Lines<BufReader<File>>) -> String {
     for line in lines {
         if let Ok(contents) = line {
             // do something with each line :)
         }
     }
 
-    "Not Started"
+    "Not Started".to_owned()
 }
 
 // Use this function to solve part two of the puzzle
 // Return the answer as a string
-pub fn part_two(lines: Lines<BufReader<File>>) -> &'static str {
+pub fn part_two(lines: Lines<BufReader<File>>) -> String {
     // Recommend copy/paste the part one code to modify
 
-    "Not Started"
+    "Not Started".to_owned()
 }
